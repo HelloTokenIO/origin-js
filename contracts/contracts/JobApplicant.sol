1ea4dc6eedbedcb9baee8e042d0a71c2453043d2
Waheed Rahuman <waheed.rahuman@gmail.com>
2018-07-01 21:41:13 +0400

    Update JobApplicant.sol

--- contracts/contracts/JobApplicant.sol
+++ contracts/contracts/JobApplicant.sol
@@ -1,1 +1,51 @@
-//
+pragma solidity 0.4.23;
+
+/// @title JobApplicant
+/// @dev An purchase Origin Listing representing a purchase/booking
+import "./Listing.sol";
+
+
+contract JobApplicant {
+
+  /*
+  * Events
+  */
+
+  event JobApplicationChange(Stages stage);
+  event JobApplicationReview(address reviewer, address reviewee, Roles revieweeRole, uint8 rating, bytes32 ipfsHash);
+
+  /*
+  * Enum
+  */
+
+  enum Stages {
+
+    // Applicant Status
+    AWAITING_INTERVIEW,			// Applicant applied for interview and waiting for the interview
+    AWAITING_JOBOFFER,			// Applicant attended the interview and waiting for the job offer
+    CANCEL_APPLICATION,			// Applicant cancelled the job application, after applying for the job
+
+    // Employer Status
+    INTERVIEW_PENDING,			// Employer reviewed the application and send interview call and waiting for interview acceptance
+    JOBOFFER_PENDING,			// Employer finished interview and sent job offer and waiting for acceptance from the applicant
+    APPLICATION_REJECTED,		// Applicant not eligible for the job
+
+    EMPLOYEMENT_PERIOD,			// Both Employer and Applicant agreed and Job in progress
+
+    IN_DISPUTE,				// We are in a dispute between employer and employee on payments    
+    EMPLOYEMENT_TERMINATION,		// End of Job Contract
+
+  }
+
+  enum Roles {
+    APPLICANT,
+    EMPLOYER
+  }
+
+  /*
+  * Storage
+  */
+
+  Stages private internalStage = Stages.AWAITING_INTERVIEW;
+  
+}
